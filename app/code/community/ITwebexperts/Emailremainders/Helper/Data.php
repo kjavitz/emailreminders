d3328587909d473c6f67fe6073a48f57eae17c03
Anton Vlasenko <vlasenko.anton@gmail.com>
2014-02-12 20:22:54 +0300

    Initial commit.

---
+++ app/code/community/ITwebexperts/Emailremainders/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+class ITwebexperts_Emailremainders_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
+	 
---
+++ app/code/community/ITwebexperts/Emailremainders/Model/Observer.php
@@ -0,0 +1,13 @@
+<?php
+class ITwebexperts_Emailremainders_Model_Observer
+{
+    /**
+     * Adds "Emails" tab to the customer edit page in admin.
+     *
+     * @param Varien_Event_Observer $observer
+     */
+    public function onCustomerEmailsTabAdd(Varien_Event_Observer $observer)
+    {
+
+    }
+}

e6873eb2062c8ee08d7140daa6af770eedf57370
Anton Vlasenko <vlasenko.anton@gmail.com>
2014-02-13 01:14:54 +0300

    A new 'Emails' tab added to the customer edit page in admin. We need it to display all the emails that have been sent to the customer.

--- app/code/community/ITwebexperts/Emailremainders/Model/Observer.php
+++ app/code/community/ITwebexperts/Emailremainders/Model/Observer.php
@@ -1,4 +1,5 @@
 <?php
+
 class ITwebexperts_Emailremainders_Model_Observer
 {
     /**
@@ -8,6 +9,23 @@ class ITwebexperts_Emailremainders_Model_Observer
      */
     public function onCustomerEmailsTabAdd(Varien_Event_Observer $observer)
     {
+        $block = $observer->getEvent()->getBlock();
+        if (!($block instanceof Mage_Adminhtml_Block_Customer_Edit_Tabs)) {
+            return;
+        }
+        $block->addTabAfter('emailremainders', array(
+           'label' => $this->getModuleHelper()->__('Emails'),
+            'content' => '',
+        ), 'tags');
+    }
 
+    /**
+     * Returns default module helper.
+     *
+     * @return ITwebexperts_Emailremainders_Helper_Data
+     */
+    public function getModuleHelper()
+    {
+        return Mage::helper('emailremainders');
     }
 }

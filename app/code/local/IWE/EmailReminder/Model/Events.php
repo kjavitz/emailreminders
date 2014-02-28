e75c82bc5a32a6aabcd849111f9ff01ef9340630
Nam Nguyen <namchicken@gmail.com>
2014-02-28 23:03:14 +0700

    Email Reminder extension

--- app/code/local/IWE/EmailReminder/Model/Events.php
+++ app/code/local/IWE/EmailReminder/Model/Events.php
@@ -49,9 +49,24 @@ class IWE_EmailReminder_Model_Events extends AW_Followupemail_Model_Events
     public function quoteProcessed($eventData){
         $ruleIds = Mage::getModel('followupemail/mysql4_rule')
             ->getRuleIdsByEventType(IWE_EmailReminder_Model_Source_Rule_Types::RULE_TYPE_QUOTE_PROCESSED);
+        $customerId = 2; //Need to load the customer here
         if (count($ruleIds)) {
             foreach ($ruleIds as $ruleId) {
-
+                $canProcess = true;
+                $rule = Mage::getModel('followupemail/rule')->load($ruleId);
+                $salesRep = $rule->getData('sales_rep_id');
+                if($salesRep != 0){
+                    $dealerId = Mage::getModel('amperm/perm')->getResource()->getUserByCustomer($customerId);
+                    if($dealerId != $salesRep){
+                        $canProcess = false;
+                    }
+                }
+                if($canProcess){
+                    $params = array();
+                    $objects = array();
+                    $params['customer_id'] = $customerId;
+                    Mage::getModel('iwe_emailreminder/rule')->load($ruleId)->process($params, $objects);
+                }
             }
         }
     }
